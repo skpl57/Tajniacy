508e31bc247fa83acbf6bd2c54200562ec2a2820
skpl57 <kania.skpl57@gmail.com>
2026-06-16 08:33:21 +0200

    test

--- app/src/main/java/com/example/tajniacy/MainActivity.java
+++ app/src/main/java/com/example/tajniacy/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.tajniacy;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +21,9 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        Log.d("Cos", "Kocham Cię <3");
+
+
     }
 }
